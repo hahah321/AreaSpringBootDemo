68cfdfdf3cf540897c50233f8547baa2e6c7db58
zhou <1550563037@qq.com>
2021-04-17 11:20:56 +0800

    尝试更改hello路由返回值

--- src/main/java/com/example/demo/hello.java
+++ src/main/java/com/example/demo/hello.java
@@ -8,6 +8,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class hello {
     @RequestMapping(value="/hello",method = RequestMethod.GET)
     public String hello(){
-        return "Hello Spring";
+        return "Hello SpringBoot!";
     }
 }
